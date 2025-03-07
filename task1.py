b1bdaaf9110aa62ec74707a115d1346458dcc47c
Rajesh Reddy <rajeshreddyp686@gmail.com>
2025-03-07 17:52:06 +0530

    Added task1.py

---
+++ task1.py
@@ -0,0 +1,8 @@
+a1 = float(input("Enter the first number: "))
+a2 = float(input("Enter the second number: "))
+
+print("Addition:", a1 + a2)
+print("Subtraction:", a1 - a2)
+print("Multiplication:", a1 * a2)
+print("Division:", a1 / a2 if a2 != 0 else "Undefined (cannot divide by zero)")
+
